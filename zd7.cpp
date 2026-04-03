f93771ca0f369facd8393b064af7173636bccdf2
aleksandraksenov135-coder <aleksandraksenov135@gmail.com>
2026-04-03 13:01:35 +0300

    Update zd7.cpp

--- zd7.cpp
+++ zd7.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int a, b, operation;
+    cin >> a >> b >> operation;
+    
+    switch (operation) {
+        case 1:
+            cout << a + b;
+            break;
+        case 2:
+            cout << a - b;
+            break;
+        case 3:
+            cout << a * b;
+            break;
+        case 4:
+            if (b == 0) {
+                cout << "Деление на ноль";
+            } else {
+                cout << a / b;
+            }
+            break;
+        default:
+            cout << "Ошибка";
+    }
+    
+    return 0;
+}
