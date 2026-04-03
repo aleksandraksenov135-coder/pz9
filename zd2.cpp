966876271372a412e0f5764cf77f226beeacb737
aleksandraksenov135-coder <aleksandraksenov135@gmail.com>
2026-04-03 12:59:53 +0300

    Update zd2.cpp

--- zd2.cpp
+++ zd2.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int day;
+    cin >> day;
+    
+    switch (day) {
+        case 1:
+            cout << "Понедельник";
+            break;
+        case 2:
+            cout << "Вторник";
+            break;
+        case 3:
+            cout << "Среда";
+            break;
+        case 4:
+            cout << "Четверг";
+            break;
+        case 5:
+            cout << "Пятница";
+            break;
+        case 6:
+            cout << "Суббота";
+            break;
+        case 7:
+            cout << "Воскресенье";
+            break;
+        default:
+            cout << "Неверный день";
+    }
+    
+    return 0;
+}
