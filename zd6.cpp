4fe1a2db474d66ec544941dda309753ab0264380
aleksandraksenov135-coder <aleksandraksenov135@gmail.com>
2026-04-03 13:01:26 +0300

    Update zd6.cpp

--- zd6.cpp
+++ zd6.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int digit;
+    cin >> digit;
+    
+    switch (digit) {
+        case 0:
+            cout << "Ноль";
+            break;
+        case 1:
+            cout << "Один";
+            break;
+        case 2:
+            cout << "Два";
+            break;
+        case 3:
+            cout << "Три";
+            break;
+        case 4:
+            cout << "Четыре";
+            break;
+        case 5:
+            cout << "Пять";
+            break;
+        case 6:
+            cout << "Шесть";
+            break;
+        case 7:
+            cout << "Семь";
+            break;
+        case 8:
+            cout << "Восемь";
+            break;
+        case 9:
+            cout << "Девять";
+            break;
+        default:
+            cout << "Ошибка";
+    }
+    
+    return 0;
+}
