ff8bafa2ae4f1fbb84734b9b6a9539f363f9a3dc
aleksandraksenov135-coder <aleksandraksenov135@gmail.com>
2026-04-03 13:01:12 +0300

    Update zd5.cpp

--- zd5.cpp
+++ zd5.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int grade;
+    cin >> grade;
+    
+    switch (grade) {
+        case 5:
+            cout << "Отлично";
+            break;
+        case 4:
+            cout << "Хорошо";
+            break;
+        case 3:
+            cout << "Удовлетворительно";
+            break;
+        case 2:
+        case 1:
+            cout << "Плохо";
+            break;
+        default:
+            cout << "Ошибка";
+    }
+    
+    return 0;
+}
